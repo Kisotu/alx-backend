153308bcc405a68d940148a3e96f67c1662e6ceb
Oloishorua <jacoboloishorua@gmail.com>
2024-08-21 22:43:06 +0300

    task 3 - caching using least frequently used mechanism

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Least Recently Used Mechanism caching module.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """A class that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    """
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Stores an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets an item linked by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
